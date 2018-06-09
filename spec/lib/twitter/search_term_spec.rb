fa84b6375696eb9f1a067ee16d844afccd49756b
Hassan Zaki <hassan.zaki@badrit.com>
2018-06-09 17:54:38 +0200

    Add tests

---
+++ spec/lib/twitter/search_term_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe Twitter::SearchTerm do
+
+  context "valid data" do
+    it "initialize with default values" do
+      term = "Hello"
+      search_term = Twitter::SearchTerm.new(term)
+      expect(search_term.term).to eq(term)
+      expect(search_term.options).to eq({
+        count: Twitter::SearchTerm::DEFAULT_TWEET_COUNT,
+        result_type: Twitter::SearchTerm::DEFAULT_RESULT_TYPE
+      })
+    end
+
+    it "override default values with valid data" do
+      term = "Hello"
+      count = 5
+      result_type = "popular"
+      search_term = Twitter::SearchTerm.new(term, count: count, result_type: result_type)
+      expect(search_term.options).to eq({
+        count: count,
+        result_type: result_type
+      })
+    end
+  end
+
+  context "invali data" do
+    it "set default values if invalid data entered | negative tweet count" do
+      term = "Hello"
+      count = -5
+      result_type = "xyz"
+      search_term = Twitter::SearchTerm.new(term, count: count, result_type: result_type)
+      expect(search_term.options).to eq({
+        count: Twitter::SearchTerm::DEFAULT_TWEET_COUNT,
+        result_type: Twitter::SearchTerm::DEFAULT_RESULT_TYPE
+      })
+    end
+
+    it "set default values if invalid data entered | tweet count not a number" do
+      term = "Hello"
+      count = "hello"
+      result_type = "xyz"
+      search_term = Twitter::SearchTerm.new(term, count: count, result_type: result_type)
+      expect(search_term.options).to eq({
+        count: Twitter::SearchTerm::DEFAULT_TWEET_COUNT,
+        result_type: Twitter::SearchTerm::DEFAULT_RESULT_TYPE
+      })
+    end
+  end
+end
---
+++ spec/services/tweet/search_spec.rb
@@ -0,0 +1,43 @@
+RSpec.describe Tweet::Search do
+  before(:each){
+    @client = double('client')
+    @params = {"search" => "Hello"}
+  }
+
+  context "having result" do
+    it "return the tweets" do
+      allow(@client).to receive("search").and_return([
+        {user: {name: "User 1"}, text: "Tweet text 1", favorite_count: 1, retweet_count: 0},
+        {user: {name: "User 2"}, text: "Tweet text 3", favorite_count: 2, retweet_count: 1},
+        {user: {name: "User 3"}, text: "Tweet text 3", favorite_count: 0, retweet_count: 2},
+      ])
+      tweets = Tweet::Search.new(@params,client: @client).call
+      expect(tweets.count).to eq(3)
+    end
+  end
+
+  context "empty result" do
+    it "return no tweets if no search term provided" do
+      allow(@client).to receive("search").and_return([
+        {user: {name: "User 1"}, text: "Tweet text 1", favorite_count: 1, retweet_count: 0},
+        {user: {name: "User 2"}, text: "Tweet text 3", favorite_count: 2, retweet_count: 1},
+        {user: {name: "User 3"}, text: "Tweet text 3", favorite_count: 0, retweet_count: 2},
+      ])
+      tweets = Tweet::Search.new({}, client: @client).call
+      expect(tweets.count).to eq(0)
+    end
+
+    it "return no tweets" do
+      allow(@client).to receive("search").and_return([])
+      tweets = Tweet::Search.new(@params, client: @client).call
+      expect(tweets.count).to eq(0)
+    end
+  end
+
+  context "client is not reachable" do
+    it "raise exception if something wrong happened" do
+      allow(@client).to receive("search").and_raise("Exception")
+      expect { Tweet::Search.new(@params, client: @client).call }.to raise_error(Twitter::NotReachableError)
+    end
+  end
+end
