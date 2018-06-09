8571745bf822f48eac54fab58037b523202adf34
Hassan Zaki <hassan.zaki@badrit.com>
2018-06-09 18:24:19 +0200

    Add I18n

--- app/lib/twitter/rest_client.rb
+++ app/lib/twitter/rest_client.rb
@@ -4,8 +4,8 @@ module Twitter
       twitter_credentials = Rails.application.credentials[Rails.env.to_sym][:twitter]
 
       Twitter::REST::Client.new do |config|
-        config.consumer_key = twitter_credentials[:consumer_key] #"qQiz5UjiO1uSzAALlnt6awDRw"
-        config.consumer_secret = twitter_credentials[:consumer_secret] #"DvcJkMiUhGkNZfJxF0kx5xnwQuHk80mIMAPCTqyOKpxQ0MaF4L"
+        config.consumer_key = twitter_credentials[:consumer_key]
+        config.consumer_secret = twitter_credentials[:consumer_secret]
       end
     end
   end
--- app/services/tweet/search.rb
+++ app/services/tweet/search.rb
@@ -13,7 +13,7 @@ module Tweet
       # The client has has a bug. It doesn not return the correct count.
       @client.search(@search_term.term, @search_term.options).take(@search_term.count)
     rescue
-      raise Twitter::NotReachableError, "Twitter is not reachable at the moment, try again later!"
+      raise Twitter::NotReachableError, I18n.t("not_reachable")
     end
   end
 end
