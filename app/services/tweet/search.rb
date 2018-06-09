05033edda089d801b1981c6468561684215832d3
Hassan Zaki <hassan.zaki@badrit.com>
2018-06-09 17:53:37 +0200

    Twitter Search Service

---
+++ app/services/tweet/search.rb
@@ -0,0 +1,19 @@
+module Tweet
+  class Search
+    def initialize(params, search_term_class: Twitter::SearchTerm, client: Twitter::RestClient.client)
+      @client = client
+      @search_term = search_term_class.new(params["search"],
+                                          result_type: params["result_type"],
+                                          count: params["count"])
+    end
+
+    def call
+      return [] if @search_term.term.blank?
+
+      # The client has has a bug. It doesn not return the correct count.
+      @client.search(@search_term.term, @search_term.options).take(@search_term.count)
+    rescue
+      raise Twitter::NotReachableError, "Twitter is not reachable at the moment, try again later!"
+    end
+  end
+end
